49d64987c1f284494f8b45db5f3f2dc6bcf64f21
Siam-555 <siamshahriarhasan@gmail.com>
2024-09-01 02:06:45 +0600

    latestPost card is creating successfully

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-const latestPostsContainer = document.getElementById('latest-posts');
+const latestPostCardsContainer = document.getElementById('latest-posts');
 
-async function handleLatestPosts() {
+async function createLatestPosts() {
   const latestPosts = await (await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')).json();
-  createLatestPosts(latestPosts);
-}
-
-const createLatestPosts = (latestPosts) => {
-  const cardContainer = document.createElement('div');
-  cardContainer.classList.add('grid', 'grid-cols-3');
-  latestPosts.map(latestpost => {
-    const card = document.createElement('div');
-    card.classList.add('card', 'bg-gray-50', 'w-full', 'shadow-sm');
-    card.innerHTML =  `
-        <figure class="px-6 pt-6">
-          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" class="rounded-xl" />
-        </figure>
-        <div class="card-body items-center text-center">
-          <h2 class="card-title">Shoes!</h2>
+  
+  latestPosts.map(latestPost => {
+    console.log(latestPost);
+    const card = `
+      <div class='p-5 bg-gray-50 w-full border border-gray-300 rounded-2xl shadow-sm'>
+        <img class="rounded-2xl" src="${latestPost.cover_image}" />
+        <div>
+          <p class='p-1'>${latestPost.author?.posted_date || 'No publish date'}</p>
+          <h2 class="text-lg text-black font-semibold">${latestPost.title}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
         </div>
-      `;
-    cardContainer.appendChild(card);
-  })
-  
-  latestPostsContainer.appendChild(cardContainer);
+      </div>`;
+      latestPostCardsContainer.insertAdjacentHTML("beforeend", card);
+    })
 }
 
-handleLatestPosts();
+createLatestPosts();
